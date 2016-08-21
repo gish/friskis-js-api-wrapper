ef83afb363f44158ec9d8d8457a410a0ad83d51f
Erik Hedberg <erik@gish.se>
2016-08-21 20:30:04 +0200

    Fix indentation

--- src/lib/api.js
+++ src/lib/api.js
@@ -26,77 +26,75 @@ const doApiRequest = (method, url, apikey, username, password, qs) => {
   })
 }
 
-
-
 const apiHandler = (options) => {
-    const {
-        username,
-        password,
-        apikey
-    } = options
+  const {
+      username,
+      password,
+      apikey
+  } = options
 
-    return {
-      getAuthToken (options) {
-        const url = `${baseUrl}/persons.json`
-        const method = 'GET'
+  return {
+    getAuthToken (options) {
+      const url = `${baseUrl}/persons.json`
+      const method = 'GET'
 
-        return new Promise((resolve, reject) => {
-          doApiRequest(method, url, apikey, username, password, {
-            requestauthtoken: true
-          })
-          .then((body) => {
-            const authToken = body.persons[0].authtoken
-            resolve(authToken)
-          })
+      return new Promise((resolve, reject) => {
+        doApiRequest(method, url, apikey, username, password, {
+          requestauthtoken: true
         })
-      },
-      getActivities (options) {
-        const {
-          startdate,
-          enddate,
-          businessunitids,
-          includeBooking
-        } = options
-        const url = `${baseUrl}/activities.json`
-        const method = 'GET'
+        .then((body) => {
+          const authToken = body.persons[0].authtoken
+          resolve(authToken)
+        })
+      })
+    },
+    getActivities (options) {
+      const {
+        startdate,
+        enddate,
+        businessunitids,
+        includeBooking
+      } = options
+      const url = `${baseUrl}/activities.json`
+      const method = 'GET'
 
-        return doApiRequest(method, url, apikey, username, password, {
-          businessunitids: businessunitids,
-          startdate: startdate,
-          enddate: enddate,
-          includebooking: includeBooking
-        })
-      },
-      createBooking (options) {
-        const {
-          activityid
-        } = options
-        const url = `${baseUrl}/activitybookings.json`
-        const method = 'POST'
+      return doApiRequest(method, url, apikey, username, password, {
+        businessunitids: businessunitids,
+        startdate: startdate,
+        enddate: enddate,
+        includebooking: includeBooking
+      })
+    },
+    createBooking (options) {
+      const {
+        activityid
+      } = options
+      const url = `${baseUrl}/activitybookings.json`
+      const method = 'POST'
 
-        return doApiRequest(method, url, apikey, username, password, {
-          activityid
-        })
-      },
-      deleteBooking (options) {
-        const {
-          id,
-          type
-        } = options
-        const url = `${baseUrl}/activitybookings.json`
-        const method = 'DELETE'
+      return doApiRequest(method, url, apikey, username, password, {
+        activityid
+      })
+    },
+    deleteBooking (options) {
+      const {
+        id,
+        type
+      } = options
+      const url = `${baseUrl}/activitybookings.json`
+      const method = 'DELETE'
 
-        return doApiRequest(method, url, apikey, username, password, {
-          id,
-          type
-        })
-      },
-      getBookings (options) {
-        return new Promise((resolve, reject) => {
-          reject('not implemented')
-        })
-      }
+      return doApiRequest(method, url, apikey, username, password, {
+        id,
+        type
+      })
+    },
+    getBookings (options) {
+      return new Promise((resolve, reject) => {
+        reject('not implemented')
+      })
     }
+  }
 }
 
 export default apiHandler
